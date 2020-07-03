67fd23c3ea3f163ac7d188d7087e9bf0e624e033
Jake TyCyn <jaketycyn@gmail.com>
2020-07-03 10:44:10 -0700

    Creating Search/Add to Unranked Form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,50 @@
 import React from "react";
 
 import MovieList from "./features/movielist/MovieList";
+import RankedItemDisplay from "./features/movielist/RankedItemsDisplay";
+import CreateList from "./features/create/CreateList";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <MovieList />
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/createList">Add to List</Link>
+            </li>
+            <li>
+              <Link to="/rankList">Rank List</Link>
+            </li>
+            <li>
+              <Link to="/displayList">Display List</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/createList">
+            <CreateList />
+          </Route>
+          <Route path="/rankList">
+            <MovieList />
+          </Route>
+          <Route path="/displayList">
+            <RankedItemDisplay />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
 }
 
 export default App;
---
+++ src/features/create/CreateList.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+//https://github.com/react-hook-form/react-hook-form/tree/master/examples
+//Using on blur for when users click out of the 'search box' to click on items to add to their unranked list
+
+function CreateList() {
+  const { register, errors, handleSubmit } = useForm({
+    mode: "onBlur",
+  });
+
+  const onSubmit = (data) => {
+    alert(JSON.stringify(data));
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor="searchAdd">Search</label>
+          <input
+            name="searchAdd"
+            placeholder="itemTitle"
+            ref={register({ required: true })}
+          />
+          {errors.searchAdd && <p>Can't submit blank</p>}
+        </div>
+        <input type="submit" />
+      </form>
+    </div>
+  );
+}
+
+export default CreateList;
