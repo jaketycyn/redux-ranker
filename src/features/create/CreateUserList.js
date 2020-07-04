ce87d52a582e2d9cf9c8c49becc0498148956d73
Jake TyCyn <jaketycyn@gmail.com>
2020-07-04 13:01:30 -0700

    Component Mission Statement/Github Check

---
+++ src/features/create/CreateUserList.js
@@ -0,0 +1,2 @@
+//new test component for user creation of their own personalized list
+//purpose/intent - facilitate a user searching for items they will want to rank in ranking components (currently titled movielist)
