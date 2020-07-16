cc232a9a1e8a31aafd40e5971a7eac8fe0f11d4b
Jake TyCyn <jaketycyn@gmail.com>
2020-07-16 14:46:38 -0700

    Basic Grid Card setup with material UI

--- src/display/components/ItemGrid.js
+++ src/display/components/ItemGrid.js
@@ -1,19 +1,57 @@
 import React from "react";
 import ItemGridCard from "./ItemGridCard";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
 
 //passdown via props specific information needed by the ItemGridCard for displaying
 
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+  },
+  cardMedia: {
+    paddingTop: "56.25%", // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
+
 const ItemGrid = ({ isLoading, items }) => {
+  const classes = useStyles();
   return isLoading ? (
     <h1>Loading...</h1>
   ) : (
-    <section>
-      {items.map((item) => (
-        <div>
-          <ItemGridCard title={item.title} backImg={item.poster_path} />
-        </div>
-      ))}
-    </section>
+    <Container className={classes.cardGrid} maxWidth="md">
+      <Grid container space={4}>
+        {items.map((item) => (
+          <Grid item key={item} xs={12} sm={6} md={4} lg={3}>
+            <ItemGridCard title={item.title} backImg={item.poster_path} />
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 };
 
--- src/display/components/ItemGridCard.js
+++ src/display/components/ItemGridCard.js
@@ -1,11 +1,43 @@
 import React from "react";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: "100%",
+    display: "flex",
+    flexDirection: "column",
+  },
+  cardMedia: {
+    paddingTop: "56.25%", // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
+  },
+}));
 
 const ItemGridCard = ({ title, backImg }) => {
+  const classes = useStyles();
   return (
-    <div>
-      <h1>{title}</h1>
-      <img src={"https://image.tmdb.org/t/p/w220_and_h330_face/" + backImg} />
-    </div>
+    <Card className={classes.card}>
+      <CardMedia
+        image={"https://image.tmdb.org/t/p/w220_and_h330_face/" + backImg}
+        title={title}
+        className={classes.cardMedia}
+      />
+      <CardContent className={classes.cardContent}>
+        <Typography gutterBottom variant="h5" component="h2">
+          {title}
+        </Typography>
+      </CardContent>
+    </Card>
   );
 };
 
