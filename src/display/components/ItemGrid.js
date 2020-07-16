74e96c33943cb04f68a4a2fc0cf6466ec45a765a
Jake TyCyn <jaketycyn@gmail.com>
2020-07-16 14:53:46 -0700

    MUI styles comprised in one file

--- src/display/components/ItemGrid.js
+++ src/display/components/ItemGrid.js
@@ -3,40 +3,9 @@ import ItemGridCard from "./ItemGridCard";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
 import { makeStyles } from "@material-ui/core/styles";
+import useStyles from "../styles/MUIstyles";
 
 //passdown via props specific information needed by the ItemGridCard for displaying
-
-const useStyles = makeStyles((theme) => ({
-  icon: {
-    marginRight: theme.spacing(2),
-  },
-  heroContent: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(8, 0, 6),
-  },
-  heroButtons: {
-    marginTop: theme.spacing(4),
-  },
-  cardGrid: {
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8),
-  },
-  card: {
-    height: "100%",
-    display: "flex",
-    flexDirection: "column",
-  },
-  cardMedia: {
-    paddingTop: "56.25%", // 16:9
-  },
-  cardContent: {
-    flexGrow: 1,
-  },
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6),
-  },
-}));
 
 const ItemGrid = ({ isLoading, items }) => {
   const classes = useStyles();
--- src/display/components/ItemGridCard.js
+++ src/display/components/ItemGridCard.js
@@ -3,25 +3,7 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
-import { makeStyles } from "@material-ui/core/styles";
-
-const useStyles = makeStyles((theme) => ({
-  cardGrid: {
-    paddingTop: theme.spacing(8),
-    paddingBottom: theme.spacing(8),
-  },
-  card: {
-    height: "100%",
-    display: "flex",
-    flexDirection: "column",
-  },
-  cardMedia: {
-    paddingTop: "56.25%", // 16:9
-  },
-  cardContent: {
-    flexGrow: 1,
-  },
-}));
+import useStyles from "../styles/MUIstyles";
 
 const ItemGridCard = ({ title, backImg }) => {
   const classes = useStyles();
