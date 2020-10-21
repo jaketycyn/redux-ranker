9df65e9183caf83781419d0612e7b1c5275157be
Jake TyCyn <jaketycyn@gmail.com>
2020-10-20 21:19:08 -0700

    Fixing index/connection issues

--- server/db/index.js
+++ server/db/index.js
@@ -2,13 +2,7 @@ require("dotenv").config();
 
 const { Pool } = require("pg");
 
-const isProduction = process.env.NODE_ENV === "production";
-
-const connectionString  = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.envPGDATABASE}`
-
-const pool = new Pool({
-  connectionString: isProduction ? process.env.PGDATABASE
-});
+const pool = new Pool();
 
 module.exports = {
   query: (text, params) => pool.query(text, params),
