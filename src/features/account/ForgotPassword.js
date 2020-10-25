dc6f33be7a3a75845bbabbf4ea41b834ede8143f
Jake TyCyn <jaketycyn@gmail.com>
2020-10-25 00:42:22 -0700

    Completed: ForgotPassword

---
+++ src/features/account/ForgotPassword.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Formik, Field, Form, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+
+import { accountService, alertService } from '../../_services';
+
+function ForgotPassword() {
+    return (
+        <div>
+        ForgotPassword
+        </div>
+    )
+}
